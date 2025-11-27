873db8af470537d49482ab7b87413087da924dd3
BBerasainn <bb@bsmartlab.com>
2025-11-27 00:40:48 -0300

    fix del fix

--- resources/vite.config.js
+++ resources/vite.config.js
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "./",         
-  base: "./",          
+  root: ".",             
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-  },
+    emptyOutDir: true
+  }
 });
